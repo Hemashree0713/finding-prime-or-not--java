bde4661e91b47cc01998de7c4b3ae6dd315006ea
megha-rose <meghakayathinkara@gmail.com>
2025-02-03 14:35:05 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -6,11 +6,11 @@ public class Prime
     public static void main(String[] args){
       //creating a scanner object
       
-    Scanner s=new Scanner(System.in);
+    Scanner b=new Scanner(System.in);
       
       //storing the input in number variable
       
-      int number=s.nextInt();
+      int number=b.nextInt();
       int c=0;//assign c (count value) as 0
       if(number<1){
 System.out.print("Not a prime number");}
